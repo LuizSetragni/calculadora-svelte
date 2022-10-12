e1aa44b45138a1ba692bbbeb059a14f1cc4ccc31
Luiz Setragni <luiz.setragni@gmail.com>
2022-10-11 22:30:15 -0300

    Parte de layout

---
+++ src/components/Botao.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let texto: string;
+    export let triplo: boolean = false;
+    export let duplo: boolean = false;
+    export let operacao: boolean = false;
+    export let destaque: boolean = false;
+</script>
+
+<button class="botao" 
+    class:triplo class:duplo class:destaque class:operacao>
+    {texto}
+</button>
+
+<style>
+    .botao{
+        flex-basis: 25%;
+        margin: 0;
+        border: 1px solid var(--cor-fundo);
+    }
+
+    .duplo{
+        flex-basis: 50%;
+    }
+
+    .triplo{
+        flex-basis: 75%;
+    }
+
+    .destaque{
+        background-color: red;
+        color: white;
+    }
+
+    .operacao{
+        background-color: orange;
+        color: white;
+    }
+</style>
---
+++ src/components/Tela.svelte
@@ -0,0 +1,25 @@
+<script lang=ts>
+    export let valor: string;
+    $: tamanhoFonte = valor.length > 20 ? 'pequena' : `s-${valor.length}`
+</script>
+
+<div class={`tela ${tamanhoFonte}`}>
+    <span>{valor}</span>
+</div>
+
+<style>
+    .tela{
+        display: flex;
+        background-color: aliceblue;
+        flex: 1;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 0px 5px;
+        overflow: hidden;
+        font-size: 1.7rem;
+        margin: 2px 1px;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+
+    }
+</style>
